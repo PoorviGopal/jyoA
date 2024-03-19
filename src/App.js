22aad111025afa1d10fb957444dea816a8bf3ed2
Poorvi <poorviadt@gmail.com>
2024-03-19 13:01:03 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -8,13 +8,11 @@ import { useState } from 'react';
 function App() {
   return (
     <div className="App">
-      <Router>
         <Routes>
           <Route path="/" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/home" element={<Home/>}/>
         </Routes>
-      </Router>
     </div>
   );
 }
